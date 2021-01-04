51e945856cfdd307b8b1024982c03e28077675fe
Manh Cuong <cuong@radioactive.sg>
2021-01-04 11:33:20 +0800

    add block khac

--- app/Admin/Controllers/Constant.php
+++ app/Admin/Controllers/Constant.php
@@ -7,10 +7,10 @@ abstract class Constant
     const PAGE_STATUS = array(-1 => 'Xoá', 0 => 'Đã tạo', 1 => "Đã duyệt");
     const TELCO = array( 0 => 'Viettel', 1 => "Vinafone", 2 => "Mobifone", 3 => "Khác");
     const BLOCK = array( 0 => 'N02 T3', 1 => "N03 T3-T4", 2 => "N03 T1", 3 => "N01 T4", 4 => "N02 T1", 5 => "N03 T2", 6 => "N01 T1",
-7 => "N04 AA-CC", 8 => "N01 T5");
+7 => "N04 AA-CC", 8 => "N01 T5", 100=> "Khác");
     const CUSTOMER_STATUS = array( 0 => 'Chưa liên hệ', 1 => "Chưa có nhu cầu", 2 => "Đã di chuyển", 3 => "Đang hẹn lên",
     4 => "Đã mua", 5 => "Khác");
-    const SOURCE = array(0 => 'Excel', 1 => 'Facebook', 2 => 'Page', 3 => 'Voucher', 4 => 'PR', 5 => 'WI', 6 => 'Hội viên', 7 => "Khác");
+    const SOURCE = array(0 => 'Excel', 1 => 'Facebook', 2 => 'Page', 3 => 'Voucher', 4 => 'PR', 5 => 'WI', 6 => 'Hội viên', 100 => "Khác");
     const SHOW_STATUS = array(0 => 'Ẩn', 1 => 'Hiện');
     const SWITCH_STATE = array(
         'on'  => ['value' => 1, 'text' => '&nbsp;Hiện  ', 'color' => 'success'],
